05a280351fcd9c448cc322225c78057659f5b0e8
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-12-06 19:00:00 +0530

    Add files via upload

---
+++ BankWebApp/Bank.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.25;
+
+contract Bank
+{
+    int balance;
+    
+    constructor() public
+    {
+        balance = 1;
+    }
+    
+    function getBalance() view public returns(int)
+    {
+        return balance;
+    }
+    
+    function withdraw(int amount) public
+    {
+        balance = balance - amount;
+    }
+    
+    function deposit(int amount) public
+    {
+        balance = balance + amount;
+    }
+}
